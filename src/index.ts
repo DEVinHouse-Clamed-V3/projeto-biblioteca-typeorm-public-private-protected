614dabdb873cbe7d45f172c1af12170f48829511
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-01-13 18:05:12 -0300

    Feature: adiciona a rota para post Feature: adiciona requisicao para index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "reflect-metadata";
 import express from "express";
 import cors from "cors";
-
+import axios from 'axios'
 import { AppDataSource } from "./database/data-source";
 
 import livroRoutes from "./routes/livro.routes";
@@ -10,6 +10,7 @@ import autorRoutes from "./routes/autor.routes";
 import leitorRoutes from "./routes/leitor.routes";
 
 const app = express();
+
 
 app.use(cors());
 
@@ -30,3 +31,19 @@ app.use("/leitores", leitorRoutes);
 app.listen(3333, () => {
   console.log("Servidor rodando na porta 3333");
 });
+
+const requestData = {
+  titulo: 'Test Product',
+  descricao: 'This is a test product description',
+  price: 19.99,
+  data_publicacao:'11/11/1990',
+  isbn: 'teste',
+  page_count: 333,
+  linguagem: 'portugues',
+  
+};
+
+axios.post('http://localhost:3333/livros/AdicionarLivros',requestData)
+.then((response) => console.log("sucess"))
+.catch((error) => console.log("error"))
+
--- src/routes/livro.routes.ts
+++ src/routes/livro.routes.ts
@@ -1,7 +1,48 @@
 import { Request, Response, Router } from 'express';
+import Livro from '../entities/Livro';
+import { AppDataSource } from '../database/data-source';
+
+
 
 const livroRoutes = Router();
+const livroRepository = AppDataSource.getRepository(Livro)
 
+// Home page route.
+livroRoutes.get("/", async (Request, Response) => {
+    Response.send('Oiiiiiiii')
+  });
 
+livroRoutes.get("/BuscarLivros", async (Request, Response) => {
+    const livros_banco_dados = await AppDataSource .getRepository(Livro)
+
+    const livros = await livros_banco_dados.find()
+    Response.json(livros)
+  });
+
+livroRoutes.post("/AdicionarLivros", async(req: Request, res: Response) =>
+{
+  if(!req.body.title)
+    {
+        return res.status(400).json({message: 'O titulo é obrigatorio'})
+    }
+
+  const livro = new Livro()
+  livro.titulo = req.body.title
+
+  try
+  {
+    const livro_salvo = await livroRepository.save(livro)
+    return res.status(200).json(livro_salvo)
+  }
+  catch
+  {
+    return res.status(500).json({message: 'Erro'})
+  }
+  
+
+  
+}
+
+)
 
 export default livroRoutes;
