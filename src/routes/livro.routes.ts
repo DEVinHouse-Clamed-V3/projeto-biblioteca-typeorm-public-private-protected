4b8051cd8725dff591ed95d25e792ea205c7bc21
MuriloCarvG12 <MuriloCarvalho2583@Outlook.com>
2025-01-15 13:29:19 -0300

    Fix: Remove a Rota '/' que foi utilizada para testes

--- src/routes/livro.routes.ts
+++ src/routes/livro.routes.ts
@@ -7,11 +7,6 @@ import { AppDataSource } from '../database/data-source';
 const livroRoutes = Router();
 const livroRepository = AppDataSource.getRepository(Livro)
 
-// Home page route.
-livroRoutes.get("/", async (Request, Response) => {
-    Response.send('Oiiiiiiii')
-  });
-
 
 // rota para todos os livros!
 livroRoutes.get("/Livros", async (Request, Response) => {
@@ -73,8 +68,46 @@ livroRoutes.post("/AdicionarLivros", async(req: Request, res: Response) =>
     return res.status(500).json({ message: 'Erro ao salvar livro'});
   }
   
-}
+})
 
-)
+// rota para atualizar um livro
+
+livroRoutes.put('/atualizarLivro/:livro_id', async(req, res) => 
+  {
+    const req_body = req.body
+    const id_livro = req.params.livro_id
+    
+
+    const livro_atualizar = await livroRepository.findOneBy
+    ({
+      id: parseInt(id_livro), 
+    });
+
+    if(!livro_atualizar)
+      {
+        return res.status(500).json({ message: 'O livro não foi encontrado!'});
+      }
+
+    livro_atualizar.titulo = req_body.titulo || livro_atualizar.titulo
+    livro_atualizar.descricao = req_body.descricao || livro_atualizar.descricao 
+    livro_atualizar.data_publicacao = req_body.data_publicacao || livro_atualizar.data_publicacao
+    livro_atualizar.isbn = req_body.isbn || livro_atualizar.isbn
+    livro_atualizar.page_count = req_body.page_count || livro_atualizar.page_count
+    livro_atualizar.linguagem = req_body.linguagem || livro_atualizar.linguagem
+    livro_atualizar.created_at = req_body.created_at || livro_atualizar.created_at
+    livro_atualizar.updated_at = req_body.updated_at || livro_atualizar.updated_at
+
+   const livro_atualizado = await livroRepository.save(livro_atualizar)
+    try
+    {
+      return res.status(200).json(livro_atualizado)
+    }
+  
+    catch (error) {
+      console.error("Error saving book:", error);
+      return res.status(500).json({ message: 'Erro ao salvar livro'});
+    }
+
+  })
 
 export default livroRoutes;
