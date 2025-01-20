7d47f4320afba50d3e8d70fb9e488a8e8223fd4a
Patella <jfpatella@gmail.com>
2025-01-20 14:26:31 -0300

    Entidade autor criada

--- src/entities/Autor.ts
+++ src/entities/Autor.ts
@@ -1,5 +1,30 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('Autor')
 class Autor {
+    @PrimaryGeneratedColumn()
+    id: number
     
+    @Column()
+    name: string
+
+    @Column()
+    birthdate: Date
+
+    @Column()
+    biography: string
+
+    @Column()
+    nationality: string
+
+    @Column()
+    active: boolean
+
+    @Column()
+    created_at: Date
+
+    @Column()
+    updated_at: Date
 }
 
 export default Autor;
